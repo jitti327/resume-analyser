308f222688d3fae5a9b7d0c331deda03940c7137
jatindersemwal <jitti327@gmail.com>
2025-10-26 11:16:24 +0530

    Addiing authentication with puter.js so, that the backend works with puter.js and we don't need any db or backend manual API

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import NavBar from "~/components/NavBar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { usePuterStore } from "../../lib/puter";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +14,14 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { isLoading, auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [auth.isAuthenticated]);
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <NavBar />
